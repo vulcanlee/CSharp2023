c1fc2085d2b10cf9d69e8dd04df9fab27fd156ef
Vulcan <vulcan.lee@gmail.com>
2023-04-26 20:13:23 +0800

    csWord2Text

---
+++ csWord2Text/csWord2Text/Program.cs
@@ -0,0 +1,26 @@
+﻿using NPOI.XWPF.Extractor;
+using NPOI.XWPF.UserModel;
+using System;
+using System.IO;
+
+namespace csWord2Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 讀取 Word 檔案
+            XWPFDocument document = new XWPFDocument(File.OpenRead(@"UserGuide_Android.docx"));
+
+            // 讀取所有內容
+            XWPFWordExtractor extractor = new XWPFWordExtractor(document);
+            string text = extractor.Text;
+
+            // 輸出所有內容
+            Console.WriteLine(text);
+
+            // 關閉 C# 檔案
+            document.Close();
+        }
+    }
+}
