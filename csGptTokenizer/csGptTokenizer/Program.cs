97f9c88e9da25a7bc953942d7e9e626f2402ad42
Vulcan <vulcan.lee@gmail.com>
2023-05-02 17:56:47 +0800

    GPT3Tokenizer

--- csGptTokenizer/csGptTokenizer/Program.cs
+++ csGptTokenizer/csGptTokenizer/Program.cs
@@ -7,13 +7,13 @@ namespace csGptTokenizer
         static void Main(string[] args)
         {
             string text = "Microsoft MVP | 提供 .NET C# / Blazor / MAUI 教育訓練與顧問服務";
-            List<int> tokensChinese = GPT3Tokenizer.Encode(text);
+            List<int> tokens = GPT3Tokenizer.Encode(text);
             Console.WriteLine(text);
-            Console.WriteLine($"中文字串長度:{text.Length}, Token數量:{tokensChinese.Count}");
+            Console.WriteLine($"中文字串長度:{text.Length}, Token數量:{tokens.Count}");
             text = "Microsoft MVP | Providing .NET C# / Blazor / MAUI Education, Training and Consulting Services";
-            tokensChinese = GPT3Tokenizer.Encode(text);
+            tokens = GPT3Tokenizer.Encode(text);
             Console.WriteLine(text);
-            Console.WriteLine($"英文字串長度:{text.Length}, Token數量:{tokensChinese.Count}");
+            Console.WriteLine($"英文字串長度:{text.Length}, Token數量:{tokens.Count}");
             Console.WriteLine($"上述 Token 計算結果，可以從 https://platform.openai.com/tokenizer 來進行比對");
         }
     }
