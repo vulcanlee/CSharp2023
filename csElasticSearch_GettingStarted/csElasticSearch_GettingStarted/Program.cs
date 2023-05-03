4d3553590400fe9b0829f59f1ad980b3d1584083
Vulcan <vulcan.lee@gmail.com>
2023-05-03 13:51:37 +0800

    使用 HttpClient 呼叫 ElasticSearch 的 REST API

---
+++ csElasticSearch_GettingStarted/csElasticSearch_GettingStarted/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace csElasticSearch_GettingStarted
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await FirstQuickLaunch();
+        }
+
+        /// <summary>
+        /// 使用 HttpClient 呼叫 ElasticSearch 的 REST API
+        /// </summary>
+        /// <returns></returns>
+        private static async Task FirstQuickLaunch()
+        {
+            string url = "http://192.168.82.7:9200/";
+            string response = await new HttpClient().GetStringAsync(url);
+            Console.WriteLine(response);
+        }
+    }
+}
