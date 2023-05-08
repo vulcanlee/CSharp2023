8570c1267b3ba3a3d7836ca4b39d740ef090aad8
Vulcan <vulcan.lee@gmail.com>
2023-05-08 14:52:20 +0800

    第一次使用 C# 與 ElasticSearch

---
+++ csElasticSearchQuichStart/csElasticSearchQuichStart/Program.cs
@@ -0,0 +1,31 @@
+﻿using Elastic.Clients.Elasticsearch;
+
+namespace csElasticSearchQuichStart;
+
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        // 建立 ElasticsearchClient 物件
+        var client = new ElasticsearchClient(new Uri("http://192.168.82.7:9200"));
+
+        // 建立一個 Tweet 物件
+        var tweet = new Tweet
+        {
+            User = "Vulcan Lee",
+            PostDate = DateTime.Now,
+            Message = "Elasticsearch 是一個開源的、分布式的、RESTful 風格的搜索和分析引擎。"
+        };
+
+        // 將 Tweet 物件存入 Elasticsearch
+        var response = await client.IndexAsync(tweet, "my-index-name");
+
+        // 檢查存入是否成功
+        if (response.IsValidResponse)
+        {
+            // 取得存入的 ID
+            Console.WriteLine($"Index document with ID {response.Id} succeeded.");
+        }
+    }
+}
---
+++ csElasticSearchQuichStart/csElasticSearchQuichStart/Tweet.cs
@@ -0,0 +1,20 @@
+﻿namespace csElasticSearchQuichStart;
+
+/// <summary>
+/// Tweet 物件
+/// </summary>
+public class Tweet
+{
+    /// <summary>
+    /// 使用者名稱
+    /// </summary>
+    public string User { get; set; }
+    /// <summary>
+    /// 發文日期
+    /// </summary>
+    public DateTime PostDate { get; set; }
+    /// <summary>
+    /// 發文內容
+    /// </summary>
+    public string Message { get; set; }
+}
