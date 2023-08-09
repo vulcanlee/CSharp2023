28abba100f404d17e1aca3861adfa65459b2e528
Vulcan <vulcan.lee@gmail.com>
2023-08-09 11:23:27 +0800

    探討在沒有使用任何一種 Log 套件下，想要做到系統活動的設計做法

---
+++ csLog01/csLog01/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace csLog01
+{
+    /// <summary>
+    /// 不要發明輪子，自己做 Log 機制
+    /// 在底下的做法，你看到了甚麼問題?
+    /// </summary>
+    internal class Program
+    {
+        public static Logger logger = new Logger();
+        static void Main(string[] args)
+        {
+            logger.Trace("我是追蹤:Trace");
+        }
+    }
+
+    public class Logger
+    {
+        public void Trace(string message)
+        {
+            #region 想要將日誌訊息寫入螢幕
+            Console.WriteLine(message);
+            #endregion
+
+            #region 想要將日誌訊息寫入到檔案內
+            string filePath = "example.txt";
+
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.Write(message + Environment.NewLine);
+            }
+            #endregion
+        }
+    }
+}
