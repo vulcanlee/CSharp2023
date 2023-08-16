f0fa0a3d87ce455f74277db7bba7235676f0a6eb
Vulcan <vulcan.lee@gmail.com>
2023-08-16 13:15:06 +0800

    在 .NET Core 專案下，使用 與 NLog

---
+++ csLog03/csLog03/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace csLog03
+{
+    // 在 .NET Core 專案下，使用 與 NLog
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            try
+            {
+                var config = new ConfigurationBuilder()
+                   .SetBasePath(System.IO.Directory.GetCurrentDirectory()) //From NuGet Package Microsoft.Extensions.Configuration.Json
+                   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                   .Build();
+
+                using var servicesProvider = new ServiceCollection()
+                    .AddTransient<Runner>() // Runner is the custom class
+                    .AddLogging(loggingBuilder =>
+                    {
+                        // configure Logging with NLog
+                        loggingBuilder.ClearProviders();
+                        loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                        loggingBuilder.AddNLog(config);
+                    }).BuildServiceProvider();
+
+                var runner = servicesProvider.GetRequiredService<Runner>();
+                runner.DoAction("Action1");
+
+                Console.WriteLine("Press ANY key to exit");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                // NLog: catch any exception and log it.
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                LogManager.Shutdown();
+            }
+        }
+    }
+    public class Runner
+    {
+        private readonly ILogger<Runner> _logger;
+
+        public Runner(ILogger<Runner> logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoAction(string name)
+        {
+            _logger.LogDebug(20, "Doing hard work! {Action}", name);
+        }
+    }
+}
