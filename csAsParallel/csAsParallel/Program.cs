19aed9ce66a5c8015ef6b71cbe210551c7e4f833
Vulcan <vulcan.lee@gmail.com>
2023-09-24 20:40:34 +0800

    在 C# 內，透過 PLinq 的 AsParallel 方法做到多執行緒與平行化處理剖析

--- csAsParallel/csAsParallel/Program.cs
+++ csAsParallel/csAsParallel/Program.cs
@@ -6,17 +6,16 @@
         {
             var source = Enumerable.Range(1, 10000);
 
-            // Opt in to PLINQ with AsParallel.
-            var evenNums = from num in source.AsParallel()
-                           where num % 2 == 0
-                           select ShowInfo(num);
+            var evenNums = source.AsParallel()
+                //.WithDegreeOfParallelism(40)
+                .Select(ShowInfo);
 
             Console.WriteLine($"Total {evenNums.Count()}");
         }
 
         static int ShowInfo(int n)
         {
-            Console.WriteLine($" {DateTime.Now:mm:ss} - {n} / Thread ID {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"N={n:d6} {DateTime.Now:mm:ss} - {n} / Thread ID {Thread.CurrentThread.ManagedThreadId}");
             Thread.Sleep(5000);
             return n;
         }
