e9238f65736a60cc2584294f3e11e06f951b38c3
Vulcan <vulcan.lee@gmail.com>
2023-10-30 17:59:54 +0800

    .NET C# 使用 NLog 紀錄封存處理 切割、最多天數、壓縮

--- csLog18/csLog18/Program.cs
+++ csLog18/csLog18/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace csLog18
+﻿using NLog;
+
+namespace csLog18;
+
+internal class Program
 {
-    internal class Program
+    // 取得當前執行這個方法的類別對應的 Logger 物件
+    public static Logger logger =
+        LogManager.GetCurrentClassLogger();
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // 觀察 Log File 封存的做法?
+        Console.WriteLine($"每分鐘進行封存一次");
+
+        while (true)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"現在時間 {DateTime.Now:hh:mm:ss}");
+            var read = Console.ReadKey();
+            if (read.Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                logger.Trace("我是追蹤:Trace");
+                logger.Debug("我是偵錯:Debug");
+                logger.Info("我是資訊:Info");
+                logger.Warn("我是警告:Warn");
+                logger.Error("我是錯誤:error");
+                logger.Fatal("我是致命錯誤:Fatal");
+            }
         }
     }
 }
