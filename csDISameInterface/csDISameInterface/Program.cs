0fd3eba5986649d6a0dcae16012e0a7bf93c4eec
Vulcan <vulcan.lee@gmail.com>
2024-01-12 14:29:49 +0800

    .

--- csDISameInterface/csDISameInterface/Program.cs
+++ csDISameInterface/csDISameInterface/Program.cs
@@ -1,79 +1,78 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace csDISameInterface
+namespace csDISameInterface;
+
+public interface IService
 {
-    public interface IService
+    void Get();
+}
+public class ServiceA : IService
+{
+    public void Get()
     {
-        void Get();
+        Console.WriteLine("ServiceA");
     }
-    public class ServiceA : IService
-    {
-        public void Get()
-        {
-            Console.WriteLine("ServiceA");
-        }
-    }
+}
 
-    public class ServiceB : IService
+public class ServiceB : IService
+{
+    public void Get()
     {
-        public void Get()
-        {
-            Console.WriteLine("ServiceB");
-        }
+        Console.WriteLine("ServiceB");
     }
+}
 
-    public class ServiceC : IService
+public class ServiceC : IService
+{
+    public void Get()
     {
-        public void Get()
-        {
-            Console.WriteLine("ServiceC");
-        }
+        Console.WriteLine("ServiceC");
     }
-    internal class Program
+}
+internal class Program
+{
+    delegate IService ServiceResolver(string key);
+    static void Main(string[] args)
     {
-        delegate IService ServiceResolver(string key);
-        static void Main(string[] args)
-        {
-            var host = Host.CreateDefaultBuilder()
-                .ConfigureServices((context, services) =>
+        IHost host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                services.AddTransient<ServiceA>();
+                services.AddTransient<ServiceB>();
+                services.AddTransient<ServiceC>();
+                services.AddTransient<Func<string, IService>>(serviceProvider => key =>
                 {
-                    services.AddTransient<ServiceA>();
-                    services.AddTransient<ServiceB>();
-                    services.AddTransient<ServiceC>();
-                    services.AddTransient<Func<string, IService>>(serviceProvider => key =>
+                    return key switch
                     {
-                        return key switch
-                        {
-                            "A" => serviceProvider.GetService<ServiceA>(),
-                            "B" => serviceProvider.GetService<ServiceB>(),
-                            "C" => serviceProvider.GetService<ServiceC>(),
-                            _ => throw new KeyNotFoundException()
-                        };
-                    });
+                        "A" => serviceProvider.GetService<ServiceA>(),
+                        "B" => serviceProvider.GetService<ServiceB>(),
+                        "C" => serviceProvider.GetService<ServiceC>(),
+                        _ => throw new KeyNotFoundException()
+                    };
+                });
 
-                    services.AddTransient<ServiceResolver>(serviceProvider => key =>
+                services.AddTransient<ServiceResolver>(serviceProvider => key =>
+                {
+                    switch (key)
                     {
-                        switch (key)
-                        {
-                            case "A":
-                                return serviceProvider.GetService<ServiceA>();
-                            case "B":
-                                return serviceProvider.GetService<ServiceB>();
-                            case "C":
-                                return serviceProvider.GetService<ServiceC>();
-                            default:
-                                throw new KeyNotFoundException();
-                        }
-                    });
-                }).Build();
+                        case "A":
+                            return serviceProvider.GetService<ServiceA>();
+                        case "B":
+                            return serviceProvider.GetService<ServiceB>();
+                        case "C":
+                            return serviceProvider.GetService<ServiceC>();
+                        default:
+                            throw new KeyNotFoundException();
+                    }
+                });
+            }).Build();
 
-            var serviceAccessor1 = host.Services.GetRequiredService<Func<string, IService>>();
-            var serviceAccessor2 = host.Services.GetRequiredService<ServiceResolver>();
-            IService service = serviceAccessor1("A");
-            service.Get();
-            service = serviceAccessor2("C");
-            service.Get();
-        }
+        var serviceAccessor1 = host.Services.GetRequiredService<Func<string, IService>>();
+        var serviceAccessor2 = host.Services.GetRequiredService<ServiceResolver>();
+        IService service = serviceAccessor1("A");
+        service.Get();
+        service = serviceAccessor2("C");
+        service.Get();
     }
 }
